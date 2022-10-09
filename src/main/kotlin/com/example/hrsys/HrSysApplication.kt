981e88af58aa2e2c2715ba0e0f46df15a8704620
Tomáš Božek <56555554+XarfNao@users.noreply.github.com>
2022-10-09 19:16:57 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/hrsys/HrSysApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hrsys
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrSysApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrSysApplication>(*args)
+}
